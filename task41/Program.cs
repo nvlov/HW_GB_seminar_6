ea756e730a6e1943f158fb20763aefaa72d46dd4
Nikolay <nlovtsevich@gmail.com>
2023-06-29 20:19:57 +0300

    tasks done

--- task41/Program.cs
+++ task41/Program.cs
@@ -4,3 +4,31 @@
 
 0, 7, 8, -2, -2 -> 2
 1, -7, 567, 89, 223-> 3*/
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Clear();
+		Console.WriteLine("Enter numbers separated by spaces:");
+        string input = Console.ReadLine();
+
+        string[] numbers = input.Split(' ');
+
+        int positiveCount = 0;
+
+        foreach (string number in numbers)
+        {
+            int value;
+            if (int.TryParse(number, out value) && value > 0)
+            {
+                positiveCount++;
+            }
+        }
+
+        Console.WriteLine($"The number of positive values entered: {positiveCount}");
+    }
+}
+
--- task43/Program.cs
+++ task43/Program.cs
@@ -4,3 +4,31 @@
 значения b1, k1, b2 и k2 задаются пользователем.
 
 b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)*/
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Clear();
+		Console.WriteLine("Enter the values for Line 1 (y = k1 * x + b1):");
+        double k1 = ReadDouble("k1");
+        double b1 = ReadDouble("b1");
+
+        Console.WriteLine("Enter the values for Line 2 (y = k2 * x + b2):");
+        double k2 = ReadDouble("k2");
+        double b2 = ReadDouble("b2");
+
+        double xIntersection = (b2 - b1) / (k1 - k2);
+        double yIntersection = k1 * xIntersection + b1;
+
+        Console.WriteLine("The intersection point is: ({0}, {1})", xIntersection, yIntersection);
+    }
+
+    static double ReadDouble(string variableName)
+    {
+        Console.Write($"Enter {variableName}: ");
+        return double.Parse(Console.ReadLine()!);
+    }
+}
